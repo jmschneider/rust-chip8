2b03d7c0bd080084b5961f8934ed298e577fd7b4
Joel Schneider <joel@joelschneider.com>
2019-03-06 21:55:49 -0500

    Add a ton more opcodes

--- src/cpu.rs
+++ src/cpu.rs
@@ -73,7 +73,13 @@ impl Cpu {
 
   fn process_opcode(&mut self, opcode: u16) {
     // extract opcode parameters
+    let x = ((opcode & 0x0F00) >> 8) as usize;
+    let y = ((opcode & 0x00F0) >> 4) as usize;
+    let vx = self.v[x];
+    let vy = self.v[y];
     let nnn = opcode & 0x0FFF;
+    let kk = (opcode & 0x00FF) as u8;
+    let n = (opcode & 0x000F) as u8;
 
     // break up into nibbles
     let op_1 = (opcode & 0xF000) >> 12;
@@ -84,22 +90,68 @@ impl Cpu {
     // increment the program counter
     self.pc += 2;
 
+    // http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#3.1
     match(op_1, op_2, op_3, op_4) {
-      // CLS
+      // 00E0 - CLS
       (0, 0, 0xE, 0) => self.display.cls(),
-      // RET
+      // 00EE - RET
       (0, 0, 0xE, 0xE) => {
         self.sp -= 1;
         self.pc = self.stack[self.sp as usize];
       },
-      // JP addr
+      // 1nnn - JP addr
       (0x1, _, _, _) => self.pc = nnn,
-      // CALL addr
+      // 2nnn - CALL addr
       (0x2, _, _, _) => {
         self.stack[self.sp as usize] = self.pc;
         self.sp += 1;
         self.pc = nnn;
       },
+      // 3xkk - SE Vx, byte
+      (0x3, _, _, _) => self.pc += if vx == kk { 2 } else { 0 },
+      // 4xkk - SNE Vx, byte
+      (0x4, _, _, _) => self.pc += if vx != kk { 2 } else { 0 },
+      // 5xy0 - SE Vx, Vy
+      (0x5, _, _, 0) => self.pc += if vx == vy { 2 } else { 0 },
+      // 6xkk - LD Vx, byte
+      (0x6, _, _, _) => self.v[x] = kk,
+      // 7xkk - ADD Vx, byte
+      (0x7, _, _, _) => self.v[x] += kk,
+      // 8xy0 - LD Vx, Vy
+      (0x8, _, _, 0x0) => self.v[x] = vy,
+      // 8xy1 - OR Vx, Vy
+      (0x8, _, _, 0x1) => self.v[x] = vx | vy,
+      // 8xy2 - AND Vx, Vy
+      (0x8, _, _, 0x2) => self.v[x] = vx & vy,
+      // 8xy3 - XOR Vx, Vy
+      (0x8, _, _, 0x3) => self.v[x] = vx ^ vy,
+      // 8xy4 - ADD Vx, Vy
+      (0x8, _, _, 0x4) => {
+        let res = vx as u16  + vy as u16;
+        self.v[0xF] = if res > 0xFF { 1 } else { 0 };
+        self.v[x] = (res & 0xFF) as u8;
+      },
+      // 8xy5 - SUB Vx, Vy
+      (0x8, _, _, 0x5) => {
+        self.v[0xF] = if vx > vy { 1 } else { 0 };
+        self.v[x] = (vx - vy) as u8;
+      },
+      // 8xy6 - SHR Vx {, Vy}
+      (0x8, _, _, 0x6) => {
+        self.v[0xF] = vx & 0x1;
+        self.v[x] >>= 1;
+      },
+      // 8xy7 - SUBN Vx, Vy
+      (0x8, _, _, 0x7) => {
+        self.v[0xF] = if vy > vx { 1 } else { 0 };
+        self.v[x] = (vy - vx) as u8;
+      },
+      // 8xyE - SHL Vx {, Vy}
+      (0x8, _, _, 0xE) => {
+        self.v[0xF] = vx & 0x80;
+        self.v[x] <<= 1;
+      },
+      (_, _, _, _) => ()
     }
   }
 }
